cd852e9bc2f9187870e0432b3d83c8185850d8d8
taehoonlee <myartg@gmail.com>
2015-10-12 12:15:56 +0900

    code cleaning

--- src/caffe/layers/conv_layer.cu
+++ src/caffe/layers/conv_layer.cu
@@ -48,71 +48,53 @@ void ConvolutionLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
       bottom_diff = bottom[i]->mutable_gpu_diff();
     }
     // Bias gradient, if necessary.
-<<<<<<< HEAD
     if (this->bias_term_ && this->param_propagate_down_[1] && !this->adversarial) {
       if (this->manifold) {
         for (int n = 0; n < this->num_; ++n) {
-          this->backward_gpu_bias(this->blobs_[1]->mutable_gpu_diff2(), top[i]->gpu_diff2() + top[i]->offset(n));
-          this->backward_gpu_bias(this->blobs_[1]->mutable_gpu_diff3(), top[i]->gpu_diff3() + top[i]->offset(n));
+          this->backward_gpu_bias(this->blobs_[1]->mutable_gpu_diff2(), top[i]->gpu_diff2() + n * this->top_dim_);
+          this->backward_gpu_bias(this->blobs_[1]->mutable_gpu_diff3(), top[i]->gpu_diff3() + n * this->top_dim_);
         }
       } else {
         for (int n = 0; n < this->num_; ++n) {
-          this->backward_gpu_bias(this->blobs_[1]->mutable_gpu_diff(), top_diff + top[i]->offset(n));
+          this->backward_gpu_bias(this->blobs_[1]->mutable_gpu_diff(), top_diff + n * this->top_dim_);
         }
-=======
-    if (this->bias_term_ && this->param_propagate_down_[1]) {
-      Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
-      for (int n = 0; n < this->num_; ++n) {
-        this->backward_gpu_bias(bias_diff, top_diff + n * this->top_dim_);
->>>>>>> BVLC/master
       }
     }
     if (this->param_propagate_down_[0] || propagate_down[i]) {
       for (int n = 0; n < this->num_; ++n) {
         // gradient w.r.t. weight. Note that we will accumulate diffs.
-<<<<<<< HEAD
         if (this->param_propagate_down_[0] && !this->adversarial) {
           if (this->manifold) {
-            this->weight_gpu_gemm(bottom[i]->gpu_data() + bottom[i]->offset(n),
-                top[i]->gpu_diff2() + top[i]->offset(n), this->blobs_[0]->mutable_gpu_diff2());
-            this->weight_gpu_gemm(bottom[i]->gpu_data2() + bottom[i]->offset(n),
-                top[i]->gpu_diff3() + top[i]->offset(n), this->blobs_[0]->mutable_gpu_diff3());
+            this->weight_gpu_gemm(bottom[i]->gpu_data() + n * this->bottom_dim_,
+                top[i]->gpu_diff2() + n * this->top_dim_, this->blobs_[0]->mutable_gpu_diff2());
+            this->weight_gpu_gemm(bottom[i]->gpu_data2() + n * this->bottom_dim_,
+                top[i]->gpu_diff3() + n * this->top_dim_, this->blobs_[0]->mutable_gpu_diff3());
           } else {
-            this->weight_gpu_gemm(bottom[i]->gpu_data() + bottom[i]->offset(n),
-                top_diff + top[i]->offset(n), this->blobs_[0]->mutable_gpu_diff());
+            this->weight_gpu_gemm(bottom[i]->gpu_data() + n * this->bottom_dim_,
+                top_diff + n * this->top_dim_, this->blobs_[0]->mutable_gpu_diff());
           }
         }
         // gradient w.r.t. bottom data, if necessary.
         if (propagate_down[i]) {
           if (this->manifold) {
-            this->backward_gpu_gemm(top[i]->gpu_diff2() + top[i]->offset(n), weight,
-                bottom[i]->mutable_gpu_diff2() + bottom[i]->offset(n));
-            this->backward_gpu_gemm(top[i]->gpu_diff3() + top[i]->offset(n), weight,
-                bottom[i]->mutable_gpu_diff3() + bottom[i]->offset(n));
+            this->backward_gpu_gemm(top[i]->gpu_diff2() + n * this->top_dim_, weight,
+                bottom[i]->mutable_gpu_diff2() + n * this->bottom_dim_);
+            this->backward_gpu_gemm(top[i]->gpu_diff3() + n * this->top_dim_, weight,
+                bottom[i]->mutable_gpu_diff3() + n * this->bottom_dim_);
           } else {
-            this->backward_gpu_gemm(top_diff + top[i]->offset(n), weight,
+            this->backward_gpu_gemm(top_diff + n * this->top_dim_, weight,
                 bottom_diff + bottom[i]->offset(n));
           }
         } else if (this->adversarial) { // TAEHOON : n is the sample index
-          this->backward_gpu_gemm(top_diff + top[i]->offset(n), weight,
-              bottom_diff + bottom[i]->offset(n));
+          this->backward_gpu_gemm(top_diff + n * this->top_dim_, weight,
+              bottom_diff + n * this->bottom_dim_);
           Dtype sumsq;
-          caffe_gpu_dot(784, bottom_diff + bottom[i]->offset(n), bottom_diff + bottom[i]->offset(n), &sumsq);
+          caffe_gpu_dot(784, bottom_diff + n * this->bottom_dim_, bottom_diff + n * this->bottom_dim_, &sumsq);
           if ( sumsq < 1e-9 )
             sumsq = (Dtype) 0.0;
           else
             sumsq = (Dtype) 1.0 / sqrt(sumsq);
-          caffe_gpu_scal(784, sumsq, bottom_diff + bottom[i]->offset(n));
-=======
-        if (this->param_propagate_down_[0]) {
-          this->weight_gpu_gemm(bottom_data + n * this->bottom_dim_,
-              top_diff + n * this->top_dim_, weight_diff);
-        }
-        // gradient w.r.t. bottom data, if necessary.
-        if (propagate_down[i]) {
-          this->backward_gpu_gemm(top_diff + n * this->top_dim_, weight,
-              bottom_diff + n * this->bottom_dim_);
->>>>>>> BVLC/master
+          caffe_gpu_scal(784, sumsq, bottom_diff + n * this->bottom_dim_);
         }
       }
     }
